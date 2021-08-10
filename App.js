9a40424a86663fb666d0ad24ac35ecb186d5a0fb
iman.akbar <iman.akbar@mncgroup.com>
2021-08-10 17:40:40 +0700

    frist app

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.Avatar}>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Image style={{marginBottom:10}} source={require("./assets/batman-avatar.png")} />
+          <Text style={{ maxWidth: 200 }}>"it`s not whi I am"</Text>
+        </View>
+      </View>
+      <View style={styles.details}>
+        <View style={styles.item}>
+          <Text style={styles.label}>Name:</Text>
+          <Text style={styles.data}>Batman</Text>
+        </View>
+        <View style={styles.item}>
+           <Text style={styles.label}>Name:</Text>
+          <Text style={styles.data}>Batman</Text>
+        </View>
+        <View style={styles.item}>
+           <Text style={styles.label}>Name:</Text>
+          <Text style={styles.data}>Batman</Text>
+        </View>
+        <View style={styles.item}>
+           <Text style={styles.label}>Name:</Text>
+          <Text style={styles.data}>Batman</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
   },
+  Avatar: {
+    // backgroundColor: 'blue',
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 0.4,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+  },
+  details: {
+    backgroundColor: "white",
+    flex: 0.6,
+  },
+  item: {
+    // borderTopWidth: 1,
+    // borderTopColor: "black",
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    flex: 0.25,
+    // width:'100%'
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+
+  },
+  label: {
+    flex: 0.9,
+    color: 'orange',
+    fontSize: 32,
+    
+  },
+  data: {
+    fontSize:32,
+  }
 });
